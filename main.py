07f5fc29b99fb75fa8217bec79c50928a7abbde6
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-02-06 21:31:10 +0300

    add new changes to the files

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from crypt import methods
 from unicodedata import name
-from flask import Flask, render_template, request, session, logging, url_for, redirect
+from flask import Flask, render_template, request, session, logging, url_for, redirect, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from passlib.hash import sha256_crypt
 
-engine = create_engine("postgresql+psycopg2://moringa:Access@localhost/signup")
+engine = create_engine("postgresql+psycopg2://moringa:1234@localhost/signup")
 
 db = scoped_session(sessionmaker(bind=engine))
 app = Flask(__name__)
+app.secret_key = "1234code"
 
 
 @app.route('/')
@@ -17,12 +18,44 @@ def landing():
 
 
 #login page form
-@app.route('/signin')
+@app.route('/signin', methods=["GET", "POST"])
 def signin():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        usernamedata = db.execute(
+            "SELECT username FROM users WHERE username:=username", {
+                "username": username
+            }).fetchone()
+        passworddata = db.execute(
+            "SELECT password FROM users WHERE username:=username", {
+                "password": password
+            }).fetchone()
+
+        if usernamedata is None:
+            flash("NO username", "danger")
+            return render_template("signin.html")
+
+        else:
+            for password_data in passworddata:
+                if sha256_crypt.verify(password, password_data):
+                    flash("You are logged", "success")
+                    return redirect(url_for('photo'))
+                else:
+                    flash("Incorrect password", "danger")
+                    return render_template('signin.html')
+
     return render_template('signin.html')
 
 
-#register form functions
+#route for photo
+@app.route('/photo')
+def photo():
+    return render_template("photo.html")
+
+
+#register form functions,route for signup page
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     if request.method == "POST":
@@ -32,9 +65,26 @@ def signup():
         confirm = request.form.get("confirm")
         secure_password = sha256_crypt.encrypt(str(password))
 
+        if password == confirm:
+            db.execute(
+                "INSERT INTO users(name,username,password) VALUES(:name,:username,:password)",
+                {
+                    "name": name,
+                    "username": username,
+                    "password": secure_password
+                })
+            db.commit()
+            flash("You are registered and can login", "success")
+            return redirect(url_for('signin'))
+
+        else:
+            flash("password did not match", "danger")
+            return render_template('signup.html')
+
     return render_template('signup.html')
 
 
+#route for contact
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
